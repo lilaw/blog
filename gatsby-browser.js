19f1e3b088653df4c979e3591706ae70d7095891
Your Name <takialog@gmail.com>
2021-04-09 11:10:11 +0800

    style: include cssremedy; move style to src/css folder

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,12 @@
 // custom typefaces
 import "typeface-montserrat"
 import "typeface-merriweather"
-// normalize CSS across browsers
-import "./src/normalize.css"
+// normalize CSS across browsers from cssremedy
+import "./src/css/remedy.css"
+import "./src/css/reminder.css"
+import "./src/css/remedy.css"
 // custom CSS styles
-import "./src/style.css"
+import "./src/css/style.css"
 
 // Highlighting for code blocks
 import "prismjs/themes/prism.css"
