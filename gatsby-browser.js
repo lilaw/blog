179bcb3be3a5c274035e4b25e243457c9fdcaa0d
Your Name <takialog@gmail.com>
2021-04-10 09:38:37 +0800

    style:  add varable fonts and allows scale font size in difference screen

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,10 +3,10 @@ import "typeface-montserrat"
 import "typeface-merriweather"
 // normalize CSS across browsers from cssremedy
 import "./src/css/remedy.css"
-import "./src/css/reminder.css"
+import "./src/css/reminders.css"
 import "./src/css/remedy.css"
 // custom CSS styles
-import "./src/css/style.css"
+import "./src/css/style.scss"
 
 // Highlighting for code blocks
 import "prismjs/themes/prism.css"
